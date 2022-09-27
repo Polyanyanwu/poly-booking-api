c2b8b938b9c7b4dd5575c59206d65ee3f5eae45a
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-09-27 11:42:31 +0000

    feat: Add models for hotel room, hotel room facility, and general facility

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,33 @@
+""" Admin config and for hotel tables """
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Hotel, GeneralFacility, HotelRoom, HotelRoomFacility
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'brief_description',
+                    'full_description', 'rating', 'address', 'city',
+                    'contact_email', 'contact_name', 'contact_phone',
+                    'image_url', 'image', 'free_cancel_limit',
+                    'prepayment_needed', 'created_by'
+                    )
+
+
+@admin.register(HotelRoom)
+class HotelRoomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'hotel_id', 'room_type',
+                    'price', 'on_sale', 'quantity', 'sale_price',
+                    'breakfast_included')
+
+
+@admin.register(HotelRoomFacility)
+class HotelRoomFacilityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'hotel_id', 'room_type',
+                    'facility')
+
+
+@admin.register(GeneralFacility)
+class GeneralFacilityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'hotel_id', 'facility')
--- api/models.py
+++ api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import \
+    (MinLengthValidator, MinValueValidator, MaxValueValidator)
 # Create your models here.
 
 
@@ -21,3 +23,85 @@ class RoomType(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+class Hotel(models.Model):
+    """ Model that for storing hotel summary record
+        The hotel iD will be auto generated long value
+    """
+    name = models.CharField(max_length=200, null=False, blank=False)
+    brief_description = models.CharField(max_length=256,
+                                         null=False, blank=False)
+    full_description = models.TextField(null=False, blank=False)
+    rating = models.DecimalField(max_digits=5, decimal_places=2,
+                                 validators=[MaxValueValidator(5),
+                                             MinValueValidator(0)],
+                                 null=True, blank=True)
+    address = models.CharField(max_length=256, null=False, blank=False)
+    city = models.CharField(max_length=200, null=False, blank=False)
+    contact_email = models.EmailField(max_length=254, null=False, blank=False)
+    contact_name = models.CharField(max_length=200, null=True, blank=True)
+    contact_phone = models.CharField(max_length=50, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    free_cancel_limit = models.SmallIntegerField(null=True, blank=True)
+    prepayment_needed = models.BooleanField(default=False,
+                                            null=True, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL,
+                                   null=True, blank=True,
+                                   related_name='orders')
+
+    def __str__(self):
+        return self.name
+
+
+class HotelRoom(models.Model):
+    """ Model that for storing hotel room record
+    """
+    hotel_id = models.ForeignKey(Hotel, on_delete=models.CASCADE,
+                                 null=False, blank=False,
+                                 related_name='hotel')
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL,
+                                  null=True, blank=True,
+                                  related_name='room_type')
+    price = models.DecimalField(max_digits=10, decimal_places=2,
+                                validators=[MinValueValidator(0)])
+    on_sale = models.BooleanField(default=False, null=False, blank=False)
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2,
+                                     validators=[MinValueValidator(0)])
+    breakfast_included = models.BooleanField(default=False)
+    quantity = models.SmallIntegerField(default=0, null=False, blank=False)
+
+    def __str__(self):
+        return self.room_type
+
+
+class HotelRoomFacility(models.Model):
+    """ Model that for storing hotel room facility records
+    """
+    hotel_id = models.ForeignKey(Hotel, on_delete=models.CASCADE,
+                                 null=False, blank=False,
+                                 related_name='hotel_id')
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL,
+                                  null=True, blank=True,
+                                  related_name='hotel_room_type')
+    facility = models.ForeignKey(FacilityCode, on_delete=models.SET_NULL,
+                                 null=True, blank=True,
+                                 related_name='hotel_facility')
+
+    def __str__(self):
+        return self.facility
+
+
+class GeneralFacility(models.Model):
+    """ Model that for storing hotel-wide facility records
+    """
+    hotel_id = models.ForeignKey(Hotel, on_delete=models.CASCADE,
+                                 null=False, blank=False,
+                                 related_name='facility_hotel_id')
+    facility = models.ForeignKey(FacilityCode, on_delete=models.SET_NULL,
+                                 null=True, blank=True,
+                                 related_name='general_facility')
+
+    def __str__(self):
+        return self.facility
