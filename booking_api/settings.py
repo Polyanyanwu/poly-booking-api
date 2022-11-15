b7ab75f5015fbf1c95fbdd4e2b85925c714bfc2d
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-11-15 14:44:48 +0000

    deploy: Relocate database

--- booking_api/settings.py
+++ booking_api/settings.py
@@ -146,7 +146,7 @@ if 'test' in sys.argv:
     }
 else:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL_ELEPHANT"))
     }
 
 # Password validation
